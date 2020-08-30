245e336dac6236afbdfe568be9ea9fd523a1fa7c
electronyque <70457068+electronyque@users.noreply.github.com>
2020-08-29 21:33:28 -0400

    Update GetProducts.java

--- src/samples/java/com/rocketlabs/sellercenterapi/samples/endpoints/product/GetProducts.java
+++ src/samples/java/com/rocketlabs/sellercenterapi/samples/endpoints/product/GetProducts.java
@@ -14,9 +14,9 @@ public class GetProducts {
         /**
          * Runtime configuration
          */
-        SellerCenter.apiKey = Config.apiKey;
-        SellerCenter.userId = Config.userId;
-        SellerCenter.url = Config.url;
+        SellerCenter.apiKey = 382620c56efbdae6a477f0b87f4adc6cb0ef22c8;
+        SellerCenter.userId = electronyque@gmail.com;
+        SellerCenter.url = https://sellercenter-api.jumia.ma?Action=GetOrders&CreatedAfter=2014-02-25T23%3A46%3A11%2B00%3A00&Format=XML&Timestamp=2020-08-30T02%3A27%3A46%2B01%3A00&UserID=electronyque%40gmail.com&Version=1.0&Signature=cc6b4a244fbd1f76372935c43ad0a732eaa92ccf95d4e914e4b0c2ebfac5a68d;
 
         /**
          * Call the API
